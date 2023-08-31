d54660d228779012a82e8942ce1333948ac158bc
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-08-31 09:48:27 -0400

    atualização do filme

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,7 +1,12 @@
 export default function Filme(){
     return(
         <div className="filme_content">
-            Filme
+            <img src="" alt=""/>
+            <div className="texto_foto">
+                <h1>Titulo</h1>
+                <p className="sinope">blabla</p>
+
+            </div>
         </div>
     )
 }
