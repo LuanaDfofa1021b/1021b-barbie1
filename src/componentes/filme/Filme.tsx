66466d38a445270c94eefe5a6a22eaec528db98a
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-08-31 10:15:44 -0400

    arrumei as coisas erradas e coloquei outra foto

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,13 +1,20 @@
 'filme.css'
-export default function Filme(){
+
+type FilmeProps={
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
+
+export default function Filme(props:FilmeProps){
     return(
         <div className="filme_content">
-            <img src="/barbie.png" alt=""/>
+            <img src={props.imagem} alt=""/>
             <div className="texto_foto">
                 <h1>Titulo</h1>
-                <p className="sinopse">A jovem Barbie (Margot Robbie) vive no mundo perfeito das bonecas, chamado Barbielândia, onde todo dia é incrível. No entanto, quando ela entra em uma crise existencial, acaba sendo exilada devido às suas imperfeições.
-Agora a boneca precisa optar por viver uma aventura no mundo real dos humanos, onde aprenderá uma importante lição sobre a vida e o que realmente importa.
-    </p>
+                <p className="sinopse">
+                    {props.sinopse}
+                </p>
 
             </div>
         </div>
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -4,9 +4,11 @@ import './Main.css'
 export default function Main(){
     return(
         <main className="content-main">
-            <Filme/>
-            <Filme/>
-            
+            <Filme titulo="Barbie" sinopse="A jovem Barbie (Margot Robbie) vive no mundo perfeito das bonecas, chamado Barbielândia, onde todo dia é incrível. No entanto, quando ela entra em uma crise existencial, acaba sendo exilada devido às suas imperfeições.
+Agora a boneca precisa optar por viver uma aventura no mundo real dos humanos, onde aprenderá uma importante lição sobre a vida e o que realmente importa."
+imagem="/barbie.png"/>
+            <Filme titulo="transformes" sinopse="O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam."
+            imagem="/transformes.jpg"/>
         </main>
     )
 }
