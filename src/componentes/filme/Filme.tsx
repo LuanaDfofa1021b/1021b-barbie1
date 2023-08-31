2f1e8676a00ef08c10f8a6fa62d9dacbc50164cf
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-08-31 09:55:12 -0400

    coloquei a sinopse

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,10 +1,13 @@
+'filme.css'
 export default function Filme(){
     return(
         <div className="filme_content">
-            <img src="" alt=""/>
+            <img src="/barbie.png" alt=""/>
             <div className="texto_foto">
                 <h1>Titulo</h1>
-                <p className="sinope">blabla</p>
+                <p className="sinopse">A jovem Barbie (Margot Robbie) vive no mundo perfeito das bonecas, chamado Barbielândia, onde todo dia é incrível. No entanto, quando ela entra em uma crise existencial, acaba sendo exilada devido às suas imperfeições.
+Agora a boneca precisa optar por viver uma aventura no mundo real dos humanos, onde aprenderá uma importante lição sobre a vida e o que realmente importa.
+    </p>
 
             </div>
         </div>
