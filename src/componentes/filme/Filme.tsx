ed4944b97d7dedd6e7ac6dfea839bfa247ef83cb
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-09-05 07:15:03 -0400

    consegui arrumar a escrita nas fotos

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -11,12 +11,10 @@ export default function Filme(props:FilmeProps){
         <div className="filme_content">
             <img src={props.imagem} alt=""/>
             <div className="texto_foto">
-                <h1>Barbie</h1>
+                <h1>{props.titulo}</h1>
                 <div className="texto_foto"></div>
-                <h1>Transformes</h1>
-                <p className="sinopse">
                     {props.sinopse}
-                </p>
+
 
             </div>
         </div>
