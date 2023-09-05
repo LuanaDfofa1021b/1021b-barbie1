7bef10bc48c0626a6e00d80b0dd81a345344c588
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-09-05 08:27:39 -0400

    arrumei

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,11 +1,19 @@
 import './Header.css'
 import Logo from '../../assets/pngfind.com-barbie-logo-png-667657.png'
 function Header(){
+   let texto= "luana"
+    function mudaTexto(){
+        texto = "luana"
+    }
     return(
+        <>
         <header>
            <img src={Logo} alt="" />
             <nav>
                 <ul>
+                    <li>
+                        <input className="barrapesquisa" type ='text'/>
+                    </li>
                     <li>
                         <a href="#">Página inicial</a>
                     </li>
@@ -21,6 +29,10 @@ function Header(){
                 </ul>
             </nav>
         </header>
+        <div>
+            <p className='texto_digitado'> pesquisa : {texto}</p>
+        </div>
+        </>
     )
 }
 export default Header
