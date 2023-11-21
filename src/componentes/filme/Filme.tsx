06949bc47f48d2cbf683362cc13fc70d3796ba0e
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-11-21 11:27:01 -0400

    commit

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,41 +1,23 @@
-import React, { useEffect } from 'react';
-import './Filme.css';
-import axios from 'axios';
+import './Filme.css'
 
-type FilmeProps = {
-  titulo: string;
-  sinopse: string;
-  imagem: string;
+type FilmeProps={
+    titulo:string,
+    sinopse:string,
+    imagem:string
 }
 
-const Filme: React.FC<FilmeProps> = (props) => {
-  useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get('http://localhost:3000/filmes');
-        const moviesData = response.data.results; // Ajuste conforme a estrutura da resposta da API
-        console.log(moviesData); // Aqui você pode definir o estado com os dados recebidos
-      } catch (error) {
-        console.error('Erro ao buscar filmes:', error);
-      }
-    };
-
-    fetchMovies();
-  }, []);
-
-  return (
-    <div className="filme_content">
-      <div className="foto_filme">
-        <img src={props.imagem} alt={props.titulo} />
-      </div>
-      <div className="texto_foto">
-        <h1>{props.titulo}</h1>
-        <div className="sinopse">
-          {props.sinopse}
+export default function Filme(props:FilmeProps){
+    return(
+        <div className="filme_content">
+            <div className="foto_filme">
+                <img src={props.imagem} alt=""/>
+            </div>
+            <div className="texto_foto">
+                <h1>{props.titulo}</h1>
+                <p className="sinopse">
+                    {props.sinopse}
+                </p>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
-
-export default Filme;
